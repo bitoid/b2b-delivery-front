a6b15cbb96de6051bae7e014b97785da0c22c943
beqa <beqamaisuradze912@gmail.com>
2023-12-02 15:48:36 +0400

    add navigation

---
+++ app/(panel)/layout.tsx
@@ -0,0 +1,31 @@
+
+
+import Navigation from "@/components/Navigation";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import Link from "next/link";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "B2B Delivery",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  
+  
+
+  return (
+    <html lang="en" className="bg-gray-100">
+      <body className={inter.className}>
+      <Navigation/>
+        {children}</body>
+    </html>
+  );
+}
