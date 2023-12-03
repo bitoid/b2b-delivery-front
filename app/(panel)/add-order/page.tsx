6298b5bb46624058332d7329097f34169ab5dd34
beqa <beqamaisuradze912@gmail.com>
2023-12-03 16:40:47 +0400

    add both types of forms

--- app/(panel)/add-order/page.tsx
+++ app/(panel)/add-order/page.tsx
@@ -1,14 +1,32 @@
-import { Metadata } from 'next';
-import React from 'react'
-
-export const metadata: Metadata = {
-  title: "შეკვეთის დამატება",
-};
+"use client";
+import AddOrderForm from "@/components/AddOrderForm";
+import Form from "@/components/AddOrderForm";
+import UploadExcelForm from "@/components/UploadExcelForm";
+import { useState } from "react";
 
 export default function AddOrderPage() {
-  
+  const [manually, setManually] = useState(true);
 
   return (
-    <div>AddOrderPage</div>
-  )
+    <>
+      <div className="flex gap-3 pb-5">
+        <button
+          type="button"
+          className="rounded-full bg-white px-4 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          onClick={() => setManually(true)}
+        >
+          ხელით
+        </button>
+        <button
+          type="button"
+          className="rounded-full bg-white px-4 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          onClick={() => setManually(false)}
+        >
+          ექსელით
+        </button>
+      </div>
+
+      {manually ? <AddOrderForm /> : <UploadExcelForm />}
+    </>
+  );
 }
