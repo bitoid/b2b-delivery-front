bd732c02ed7aab1ed8c9d4542abed510350c4f32
beqa <beqamaisuradze912@gmail.com>
2024-01-25 02:13:16 +0400

    first steps

--- app/(panel)/orders/page.tsx
+++ app/(panel)/orders/page.tsx
@@ -2,6 +2,7 @@ import { ClientOrderType } from "@/types/orders";
 import { Metadata } from "next";
 import OrderTable from "@/components/Table";
 import { getCurrentUser } from "@/lib/session";
+import queryString from "query-string";
 
 export const metadata: Metadata = {
   title: "გაგზავნილი შეკვეთები",
@@ -18,10 +19,20 @@ export default async function OrdersPage({
   searchParams: ParamsType;
 }) {
   const user = await getCurrentUser();
-
+  console.log(searchParams);
   const orders: ClientOrderType[] = await getOrders(user?.token);
-  console.log(orders);
-  return <OrderTable data={orders} searchParams={searchParams} user={user} />;
+  const filteredOrders: ClientOrderType[] = await getFilteredOrders(
+    user?.token,
+    searchParams
+  );
+  return (
+    <OrderTable
+      data={orders}
+      filteredData={filteredOrders}
+      searchParams={searchParams}
+      user={user}
+    />
+  );
 }
 
 const getOrders = async (token: string | undefined) => {
@@ -39,7 +50,43 @@ const getOrders = async (token: string | undefined) => {
 
     let data = await response.json();
 
-    return data.results;
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getFilteredOrders = async (
+  token: string | undefined,
+  searchParams: any
+) => {
+  delete searchParams.pageSize;
+  delete searchParams.current;
+  console.log(
+    queryString.stringify(searchParams, {
+      arrayFormat: "comma",
+    })
+  );
+  try {
+    let response = await fetch(
+      `${process.env.API_URL}/orders?${queryString.stringify(searchParams, {
+        arrayFormat: "comma",
+      })}`,
+      {
+        cache: "no-store",
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    let data = await response.json();
+
+    return data;
   } catch (err) {
     console.log(err);
   }
