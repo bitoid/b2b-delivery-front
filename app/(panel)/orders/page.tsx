a2bc3bae8490e529348bcab4a9fe4905bf12ee8a
beqa <beqamaisuradze912@gmail.com>
2024-02-02 22:49:29 +0400

    complete part of filters

--- app/(panel)/orders/page.tsx
+++ app/(panel)/orders/page.tsx
@@ -2,6 +2,7 @@ import { ClientOrderType } from "@/types/orders";
 import { Metadata } from "next";
 import OrderTable from "@/components/Table";
 import { getCurrentUser } from "@/lib/session";
+import queryString from "query-string";
 
 export const metadata: Metadata = {
   title: "გაგზავნილი შეკვეთები",
@@ -19,14 +20,33 @@ export default async function OrdersPage({
 }) {
   const user = await getCurrentUser();
 
-  const orders: ClientOrderType[] = await getOrders(user?.token);
+  const filteredOrders: ClientOrderType[] = await getOrders(
+    user?.token,
+    searchParams
+  );
+  const orders: ClientOrderType[] = await getFilteredOrders(user?.token);
   console.log(orders);
-  return <OrderTable data={orders} searchParams={searchParams} user={user} />;
+  return (
+    <OrderTable
+      data={orders}
+      filteredOrders={filteredOrders}
+      searchParams={searchParams}
+      user={user}
+    />
+  );
 }
 
-const getOrders = async (token: string | undefined) => {
+const getOrders = async (
+  token: string | undefined,
+  searchParams: ParamsType
+) => {
+  const query = queryString.stringify(searchParams, {
+    arrayFormat: "comma",
+  });
+  console.log(query);
+
   try {
-    let response = await fetch(`${process.env.API_URL}/orders/`, {
+    let response = await fetch(`${process.env.API_URL}/orders?${query}`, {
       cache: "no-store",
       headers: {
         Authorization: `Token ${token}`,
@@ -38,7 +58,28 @@ const getOrders = async (token: string | undefined) => {
     }
 
     let data = await response.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
+const getFilteredOrders = async (token: string | undefined) => {
+  try {
+    let response = await fetch(`${process.env.API_URL}/orders`, {
+      cache: "no-store",
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    let data = await response.json();
+    console.log(data);
     return data;
   } catch (err) {
     console.log(err);
